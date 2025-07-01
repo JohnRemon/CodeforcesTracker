96d7e7ce9babbb950424a619a54deb432799cd6c
JohnRemon <john1812008@gmail.com>
2025-07-01 18:24:39 +0300

    Track unique solved problems

--- codeforces_api.py
+++ codeforces_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def get_user_info(handle):
     url = f"https://codeforces.com/api/user.status?handle={handle}"
@@ -13,3 +14,16 @@ def get_user_info(handle):
 
     return data
 
+def get_problem_info(handle):
+    data = get_user_info(handle)
+    print(type(data))
+    print(json.dumps(data, indent=2))
+    problem_names = []
+    seen_problem_names = set()
+    for item in data['result']:
+        if item['verdict'] == 'OK':
+            problem_name = item['problem']['name']
+            if problem_name not in seen_problem_names:
+                problem_names.append(problem_name)
+                seen_problem_names.add(problem_name)
+    return problem_names
--- routes.py
+++ routes.py
@@ -1,18 +1,21 @@
 from flask import render_template, request, jsonify, redirect, url_for
-from codeforces_api import get_user_info
+from codeforces_api import get_user_info, get_problem_info
 
 def setup_routes(app, db):
     @app.route('/', methods=['GET', 'POST'])
     def index():
         if request.method == 'POST':
-            #
+            #get the handle
             handle = request.form.get('handle')
             if not handle:
                 return "No Handle Provided", 400
+            #redirect to the dashboard
             return redirect(url_for('dashboard', handle=handle))
         else:
             return render_template('index.html')
     @app.route('/dashboard/<handle>', methods=['GET', 'POST'])
     def dashboard(handle):
-        data = get_user_info(handle)
+        #get user info and display it
+        data = get_problem_info(handle)
+
         return render_template('dashboard.html', handle=handle, data=data)
